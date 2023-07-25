b983af2471ce33def81325c797ec6cb13f66bc1d
maria <106377401+mariabh7@users.noreply.github.com>
2023-07-25 15:38:37 +0100

    Update tip.js

--- tip2.js
+++ tip2.js
@@ -14,8 +14,10 @@ function calculateBill() {
             // update tipAmount  and total value when pressing a key 
             window.addEventListener("keypress", () => {
                 if (people.value == 0) {
-                    console.error("number of people shouldn't be 0");
+                    tipAmount.textContent = "0.00";
+                    total.textContent = "0.00";
                 } else {
+                    console.log(tipNum);
                     let calculeTip = (bill.value * tipNum) / 100;
                     let firstTip = (calculeTip / people.value).toFixed(2);
                     let totalBill = ((+bill.value + calculeTip) / people.value).toFixed(2);
@@ -27,28 +29,21 @@ function calculateBill() {
     }
 }
 //calculate total when the user custom  a tip
-function calculateBill2() {
-    window.addEventListener("keypress", () => {
+customTip.addEventListener("keypress", () => {
+    people.addEventListener("keypress", () => {
         if (people.value == 0) {
-            console.error("number  of people shouldn't be 0");
+            console.error("number  of people shouldn't be 0 either the tip");
         } else {
+            console.log(customTip.value);
             let secondTip = (bill.value * customTip.value) / 100;
             let secondAmountTip = (secondTip / people.value).toFixed(2);
             let secondTotalBill = ((+bill.value + secondTip) / people.value).toFixed(2);
             tipAmount.textContent = secondAmountTip;
             total.textContent = secondTotalBill;
         }
-    })
-}
-window.addEventListener("click", (calculateBill(), calculateBill2()));
+    });
+});
+window.addEventListener("click", calculateBill());
 // reset all values .
-const reset = () => {
-    for (let i = 0; i < tip.length; i++) {
-        tip[i].classList.remove("tip-active");
-    }
-    tipAmount.textContent = "0.00";
-    total.textContent = "0.00";
-    bill.value = "";
-    customTip.value = "";
-    people.value = "";
-};
+const reset = () => location.reload();
+
